343002dbbebaef2b51be20c07eda53ba0058282c
Zeeckt <139068412+Zeeckt@users.noreply.github.com>
2023-07-20 21:55:00 -0300

    ⭐

---
+++ setup.bat
@@ -0,0 +1,13 @@
+@echo off
+
+pip install -U urllib3
+pip install -U requests
+pip install -U colorama
+pip install -U pandas
+pip install -U pycryptodome
+pip install -U keyboard
+pip install -U cryptography
+pip install -U inquirerpy
+pip install -U aiohttp
+
+pause
