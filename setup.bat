cf109bfc592656de5b3aba2d5ae0479847d6a28c
Zeeckt <139068412+Zeeckt@users.noreply.github.com>
2023-07-20 23:10:55 -0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -6,6 +6,7 @@ pip install -U colorama
 pip install -U pandas
 pip install -U pycryptodome
 pip install -U keyboard
+pip install -U datetime
 pip install -U cryptography
 pip install -U inquirerpy
 pip install -U aiohttp
